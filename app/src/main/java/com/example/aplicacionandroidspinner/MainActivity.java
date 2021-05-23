39303895e37eca6212a6c0a6c6ab560330262ccb
Bryan Valdez <bryan501th@gmail.com>
2021-05-23 02:57:46 -0500

    Dentro del onItemSeleted se determinó una acción según que elemento de los spinners estaban seleccionados

--- app/src/main/java/com/example/aplicacionandroidspinner/MainActivity.java
+++ app/src/main/java/com/example/aplicacionandroidspinner/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 
@@ -12,6 +13,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     EditText cajaInput, cajaOutput;
     Spinner spinnerInput, spinnerOutput;
+    int posicion1 = 1;
+    int posicion2 = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,29 +23,72 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         cajaInput = findViewById(R.id.caja_input);
         cajaOutput = findViewById(R.id.caja_output);
-        spinnerInput = findViewById(R.id.spinner_input);
-        spinnerOutput = findViewById(R.id.spinner_output);
 
+        ArrayAdapter adaptador = ArrayAdapter.createFromResource(this, R.array.spinner_conversion,
+                android.R.layout.simple_spinner_item);
+
+        spinnerInput = findViewById(R.id.spinner_input);
+        spinnerInput.setAdapter(adaptador);
         spinnerInput.setOnItemSelectedListener(this);
+
+        spinnerOutput = findViewById(R.id.spinner_output);
+        spinnerOutput.setAdapter(adaptador);
         spinnerOutput.setOnItemSelectedListener(this);
 
     }
 
-
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        /*if(parent.getId() == R.id.spinner_operaciones )
-            Log.i("MSJ --> ", "SPINNER 1");
 
-        Log.i("MSJ --> ", parent.toString());
-        Log.i("MSJ --> ", parent.getItemAtPosition(position).toString());
 
-        switch (parent.getItemAtPosition(position).toString()){
-            case "+": Toast.makeText(getBaseContext(), "A sumar se ha dicho", Toast.LENGTH_LONG).show();
-                cajaResultado.setText("resultado");
+        String texto = cajaInput.getText().toString();
+        if (texto.equals(""))
+            texto="0";
+        double numeroInput = Double.parseDouble(texto);
+        double numeroOutput=numeroInput;
+
+        if (parent.getId() == R.id.spinner_input)
+            posicion1=position;
+        if (parent.getId() == R.id.spinner_output)
+            posicion2=position;
+
+        switch (spinnerInput.getItemAtPosition(posicion1).toString()){
+            case "Centigrados":
+                switch (spinnerOutput.getItemAtPosition(posicion2).toString()){
+                    case "Fahrenheit":  numeroOutput=(numeroInput*1.8)+32;  break;
+                    case "Rankine":     numeroOutput=(numeroInput*1.8)+32+ 459.67;  break;
+                    case "Kelvin":      numeroOutput=numeroInput+ 273.15;   break;
+                    default:break;
+                }
                 break;
+            case "Fahrenheit":
+                switch (spinnerOutput.getItemAtPosition(posicion2).toString()){
+                    case "Centigrados": numeroOutput=(numeroInput-32)/1.8;break;
+                    case "Rankine":     numeroOutput=numeroInput+459.67;  break;
+                    case "Kelvin":      numeroOutput=(numeroInput+459.67)/1.8;   break;
+                    default:break;
+                }
+                break;
+            case "Rankine":
+                switch (spinnerOutput.getItemAtPosition(posicion2).toString()){
+                    case "Centigrados": numeroOutput=(numeroInput-491.67) / 1.8;break;
+                    case "Fahrenheit":  numeroOutput=numeroInput-459.67;  break;
+                    case "Kelvin":      numeroOutput=numeroInput/1.8;   break;
+                    default:break;
+                }
+                break;
+            case "Kelvin":
+                switch (spinnerOutput.getItemAtPosition(posicion2).toString()){
+                    case "Centigrados": numeroOutput=numeroInput- 273.15;  break;
+                    case "Fahrenheit":  numeroOutput=(numeroInput* 1.8) - 459.67;  break;
+                    case "Rankine":     numeroOutput=numeroInput*1.8;  break;
+                    default:break;
+                }
+                break;
+            default:break;
+        }
+        cajaOutput.setText(""+numeroOutput);
 
-        }*/
     }
 
     @Override
